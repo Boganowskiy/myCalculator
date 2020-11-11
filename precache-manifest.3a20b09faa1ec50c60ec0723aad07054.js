d8c896f6ad7965100b2a981b3b6fd2a55ffb4b7f
Boganowskiy Andrey <boganowskiy@gmail.com>
2020-11-11 17:14:12 +0300

    Updates

---
+++ precache-manifest.3a20b09faa1ec50c60ec0723aad07054.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "022c526de3dfab60362807d4529df0e3",
+    "url": "/myCalculator/index.html"
+  },
+  {
+    "revision": "d1d73370e58d314d842e",
+    "url": "/myCalculator/static/css/main.51315f9b.chunk.css"
+  },
+  {
+    "revision": "e61e51eacd7cf1f7cf9b",
+    "url": "/myCalculator/static/js/2.dda6dfbf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myCalculator/static/js/2.dda6dfbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1d73370e58d314d842e",
+    "url": "/myCalculator/static/js/main.179ac0b1.chunk.js"
+  },
+  {
+    "revision": "d58556afcc6590679b4f",
+    "url": "/myCalculator/static/js/runtime-main.970318fa.js"
+  }
+]);
--- src/functions/clearingButtons.js
+++
@@ -1,8 +0,0 @@
-export default {
-  BS: {
-    operation: (expr) => expr.slice(0, -1),
-  },
-  C: {
-    operation: (expr) => '',
-  },
-}
